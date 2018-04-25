77eb72e99e8ff30fb000fe070d0a73fe2977ef50
LUCK Romain <37153360+Softpurr@users.noreply.github.com>
2018-04-25 09:00:31 +0200

    Add files via upload

--- serveur.js
+++ serveur.js
@@ -17,29 +17,33 @@ py.stdin.end();
 
 
 var david = require('child_process').spawn,
-dav = spawn('python', ['./python_Kegg_final/newClient/DAVIDWebService_Client.py']),
+dav = david('python', ['./python_Kegg_final/newClient/DAVIDWebService_Client.py']),
+davdata = ["P24638", "Q9ERK7", "Q9JI91", "P09242", "P31750", "P05064", "O54774", "P98084", "Q03157", "Q06335", "P12023", "P04627", "P50428", "Q8VDN2", "P56480", "P97450", "Q9CQV8"]
 outdav = '';
 
-/*
-dav.stdout.on('data', function(){
-  dataString += data.toString();
+dav.stdout.on('data', function(data){
+  outdav += data.toString();
 });
 
-dav.stdin.write(JSON.stringify(data));
+dav.stdout.on('end', function(){
+  console.log(outdav);
+});
+
+dav.stdin.write(JSON.stringify(davdata));
 dav.stdin.end();
-*/
+
 
 var spawn1 = require('child_process').spawn,
-    py1    = spawn('python', ['compute_input.py']),
+    py1    = spawn1('python', ['test1.py']),
     data1 = [1,2,3,4,5,6,7,8,9],
     dataString1 = '';
 
 py1.stdout.on('data', function(data1){
   dataString1 += data1.toString();
 });
-//py.stdout.on('end', function(){
-//  console.log('Sum of numbers=',dataString);
-//});
+py1.stdout.on('end', function(){
+  console.log('Sum of numbers=',dataString1);
+});
 py1.stdin.write(JSON.stringify(data1));
 py1.stdin.end();
 
